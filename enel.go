26a0f47fe414f4fe762b4bdea6c457e6ae75fb0c
Hugo Virgilio <huvirgilio@gmail.com>
2020-10-20 01:16:06 -0300

    Finished login flow; invoice list flow;

--- enel.go
+++ enel.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/chromedp/chromedp"
@@ -20,13 +21,22 @@ var (
 )
 
 type flow struct {
-	c    context.Context
-	user userData
+	c       context.Context
+	user    userData
+	invoice invoice
+}
+
+type invoice struct {
+	dueDate string
+	value   string
+	barCode string
+	status  string
 }
 
 type userData struct {
 	email string
 	pw    string
+	name  string
 }
 
 func init() {
@@ -45,30 +55,22 @@ func main() {
 		defer cancel[i]()
 	}
 
-	flow.login()
+	err = flow.login()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
-	// value := "teste"
-	// dueDate := "string"
-	// err = chromedp.Run(ctx,
-	// 	chromedp.Navigate(`https://portalhome.eneldistribuicaosp.com.br/#/segunda-via`),
-	// 	chromedp.WaitVisible(`div.page-info`),
-	// 	chromedp.Text(`document.querySelector("#segunda-via > div.aes-section.less-padding > div:nth-child(2) > div.faturas-list-container > md-list > md-list-item > div.item.header1 > span")`, &value, chromedp.ByJSPath),
-	// 	chromedp.Text(`document.querySelector("#segunda-via > div.aes-section.less-padding > div:nth-child(2) > div.faturas-list-container > md-list > md-list-item > div.item.header4.vencimento > span")`, &dueDate, chromedp.ByJSPath),
-	// 	chromedp.Click(`document.querySelector("#segunda-via > div.aes-section.less-padding > div:nth-child(2) > div.faturas-list-container > md-list > md-list-item > div.action-group > div.item.act1.action.act-enable > span")`, chromedp.NodeVisible, chromedp.ByJSPath),
-	// 	chromedp.Sleep(10*time.Second),
-	// 	chromedp.Stop(),
-	// )
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-	// log.Printf("value %q", value)
-	// log.Printf("dueDate %q", dueDate)
+	err = flow.invoiceList()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
 
 func (flow *flow) login() error {
 	log.Println("Starting login flow")
-	output := ""
+	name := ""
 	err := chromedp.Run(flow.c,
 		chromedp.Navigate(`https://portalhome.eneldistribuicaosp.com.br/#/login`),
 		chromedp.WaitVisible(`h1.title`),
@@ -79,10 +81,64 @@ func (flow *flow) login() error {
 		chromedp.SendKeys("#senha", kb.End+flow.user.pw, chromedp.ByID),
 		chromedp.Click(`#btnLoginEmail`, chromedp.NodeVisible, chromedp.ByID),
 		chromedp.WaitVisible(`i.aes-sair`),
-		chromedp.Text(`document.querySelector("#troca-instalacao > div.user-data.layout-align-start-center.layout-column.flex-none > label.text")`, &output, chromedp.ByJSPath),
+		chromedp.Text(
+			`document.querySelector("#troca-instalacao > div.user-data.layout-align-start-center.layout-column.flex-none > label.name > strong")`,
+			&name,
+			chromedp.ByJSPath,
+		),
 	)
-	log.Println(output)
+	if strings.ToLower(name) != strings.ToLower(flow.user.name) {
+		return fmt.Errorf("Login failure; user name did not match")
+	}
+	if err == nil {
+		log.Println("Successfully logged in")
+	}
 	return err
+}
+
+func (flow *flow) invoiceList() error {
+	log.Println("Starting invoiceList flow")
+	table := ""
+	err := chromedp.Run(flow.c,
+		chromedp.Navigate("https://portalhome.eneldistribuicaosp.com.br/#/segunda-via"),
+		chromedp.WaitVisible("i.aes-sair"),
+		chromedp.WaitVisible("#segunda-via > div.aes-section.less-padding > div:nth-child(2) > div.faturas-list-container > md-list"),
+		chromedp.Text(`document.querySelector("#segunda-via > div.aes-section.less-padding > div:nth-child(2) > div.faturas-list-container > md-list > md-list-item:nth-child(2)")`, &table, chromedp.ByJSPath),
+	)
+	if err != nil {
+		return fmt.Errorf("invoiceFlow err: %v", err)
+	}
+	if strings.Contains(table, "Pendente") {
+		detailHeader := ""
+		err = chromedp.Run(flow.c,
+			chromedp.Click(
+				`document.querySelector("#segunda-via > div.aes-section.less-padding > div:nth-child(2) > div.faturas-list-container > md-list > md-list-item:nth-child(2) > div.action-group > div.item.act1.action.act-enable > span")`,
+				chromedp.NodeVisible,
+				chromedp.ByJSPath,
+			),
+			chromedp.WaitVisible(`#detalhamento > aes-content-header > div > div > h3`),
+			chromedp.Text(
+				`document.querySelector("#detalhamento > aes-content-header > div > div > h3")`,
+				&detailHeader,
+				chromedp.ByJSPath,
+			),
+		)
+		log.Printf("detailheader %v", detailHeader)
+		if err != nil {
+			return fmt.Errorf("click details err: %v", err)
+		}
+		if detailHeader != "Detalhamento de conta" {
+			return fmt.Errorf("missing header; loaded the wrong page")
+		}
+	}
+	if err == nil {
+		log.Println("Successfully selected the last listed invoice")
+	}
+	return nil
+}
+
+func (flow *flow) invoiceData() {
+
 }
 
 func newFlow(headless bool) (flow, []context.CancelFunc, error) {
@@ -102,6 +158,9 @@ func setUser() (userData, error) {
 	if len(user.pw) == 0 {
 		return user, fmt.Errorf("invalid user password; cannot be empty")
 	}
+	if len(user.name) == 0 {
+		return user, fmt.Errorf("invalid user name; cannot be empty")
+	}
 	return user, nil
 }
 
@@ -115,7 +174,11 @@ func readFile() (userData, error) {
 	}
 	user := map[string]string{}
 	err = json.Unmarshal(jsonFile, &user)
-	return userData{email: user["email"], pw: user["pw"]}, err
+	return userData{
+		email: user["email"],
+		pw:    user["pw"],
+		name:  user["name"],
+	}, err
 }
 
 func setContext(headless bool) (context.Context, []context.CancelFunc) {
@@ -132,4 +195,41 @@ func setContext(headless bool) (context.Context, []context.CancelFunc) {
 	ctx, cancel = chromedp.NewContext(ctx)
 	outputFunc = append(outputFunc, cancel)
 	return ctx, outputFunc
+}
+
+func (flow *flow) textByPath(path string) (string, error) {
+	output := ""
+	err := chromedp.Run(flow.c,
+		chromedp.Text(
+			path,
+			&output,
+			chromedp.ByJSPath,
+		),
+	)
+	if err != nil {
+		return "", fmt.Errorf("flow.textByPath err: %v", err)
+	}
+	return output, nil
+}
+
+func (flow *flow) textByID(id string) (string, error) {
+	output := ""
+	err := chromedp.Run(flow.c,
+		chromedp.Text(
+			id,
+			&output,
+			chromedp.ByID,
+		),
+	)
+	if err != nil {
+		return "", fmt.Errorf("flow.textByID err: %v", err)
+	}
+	return output, nil
+}
+
+func (flow *flow) waitVisible(something string) error {
+	log.Printf("waiting for %v", something)
+	return chromedp.Run(flow.c,
+		chromedp.WaitVisible(something),
+	)
 }
